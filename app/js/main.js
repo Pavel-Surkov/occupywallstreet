964fa85d4f1e658ab58161641552d94b39d46813
Pavel-Surkov <surkovv.pavel@gmail.com>
2021-11-25 23:31:00 +0700

    adds date logic

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,24 @@
 'use strict';
 
 // FUNCTIONS
-const initSwiper = check => {};
+const initSwiper = () => {
+	if (window.innerWidth <= 767) {
+		const teamSwiperWrapper = document.querySelector('.team-swiper__wrapper');
+		const teamSwiperSlides = Array.from(teamSwiperWrapper.children);
+
+		teamSwiperWrapper.classList.remove('team-swiper__wrapper');
+		teamSwiperSlides.forEach(slide => {
+			slide.classList.remove('team-swiper__slide');
+		});
+
+		let teamSwiper = new Swiper('.team-swiper', {
+			navigation: {
+				prevEl: '.swiper-button-prev',
+				nextEl: '.swiper-button-next'
+			}
+		});
+	}
+};
 
 const menuToggle = target => {
 	const menuBtn = target.closest('.menu-btn');
@@ -29,30 +46,70 @@ const mobileMenuClick = (target, e) => {
 	window.scrollTo(0, elementToScroll);
 };
 
-// const updateRoadmap = () => {
-// 	const roadMapItems = document.querySelectorAll('.roadmap-list__item');
+const updateRoadmap = () => {
+	const currentDate = new Date();
+	const roadMapItems = Array.from(document.querySelectorAll('.roadmap-list__item'));
 
-// 	roadMapItems.forEach(item => )
-// }
+	const roadMapDates = roadMapItems.map(item => {
+		const dateStr = item.dataset.date;
+		const eventDate = new Date(dateStr);
+
+		if (eventDate <= currentDate) {
+			item.classList.add('roadmap-list__item_active');
+		}
+	});
+};
+
+// const updateRoadmap = () => {
+// 	const months = [
+// 		'january',
+// 		'february',
+// 		'march',
+// 		'april',
+// 		'may',
+// 		'june',
+// 		'july',
+// 		'august',
+// 		'september',
+// 		'october',
+// 		'november',
+// 		'december'
+// 	];
+// 	const currentDate = new Date();
+// 	const roadMapItems = Array.from(document.querySelectorAll('.roadmap-list__item'));
+
+// 	roadMapItems.reduce((lastDateStr, item) => {
+// 		const dateStr = item.firstElementChild.textContent;
+
+// 		const [month, day] = dateStr.split(' ');
+// 		const monthNumber = months.indexOf(month) + 1;
+// 		const dayNumber = parseInt(day);
+// 		const currentYear = currentDate.getFullYear();
+
+// 		const numDateStr = `${currentYear}.${monthNumber}.${dayNumber}`;
+
+// 		let eventDay = new Date(numDateStr);
+// 		const lastDate = new Date(lastDateStr);
+
+// 		if (lastDate > eventDay) {
+// 			const correctDateStr = numDateStr.split('.').map((item, index) => {
+// 				return index === 0 ? ++item : item;
+// 			});
+// 			eventDay = new Date(correctDateStr);
+
+// 			alert(eventDay);
+// 			return correctDateStr;
+// 		}
+
+// 		alert(eventDay);
+
+// 		return numDateStr;
+// 	}, `2021.1.01`);
+// };
 
 document.addEventListener('DOMContentLoaded', () => {
-	// updateRoadmap();
-	if (window.innerWidth <= 767) {
-		const teamSwiperWrapper = document.querySelector('.team-swiper__wrapper');
-		const teamSwiperSlides = Array.from(teamSwiperWrapper.children);
-
-		teamSwiperWrapper.classList.remove('team-swiper__wrapper');
-		teamSwiperSlides.forEach(slide => {
-			slide.classList.remove('team-swiper__slide');
-		});
-
-		let teamSwiper = new Swiper('.team-swiper', {
-			navigation: {
-				prevEl: '.swiper-button-prev',
-				nextEl: '.swiper-button-next'
-			}
-		});
-	}
+	updateRoadmap();
+	initSwiper();
 });
 
 document.addEventListener('click', e => {
