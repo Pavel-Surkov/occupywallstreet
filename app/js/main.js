c0b08f4239acb50b5a59d884df644823a4139f0d
Pavel-Surkov <surkovv.pavel@gmail.com>
2021-11-25 11:20:50 +0700

    adds media to team secion except sm size

--- app/js/main.js
+++ app/js/main.js
@@ -28,4 +28,26 @@ document.addEventListener('click', e => {
 	}
 });
 
+window.addEventListener('resize', () => {
+	const windowWidth = window.innerWidth;
+
+	if (windowWidth === 767) {
+		const teamSwiperWrapper = document.querySelector('.team-swiper__wrapper');
+		const teamSwiperSlides = Array.from(teamSwiperWrapper.children);
+
+		teamSwiperWrapper.classList.remove('team-swiper__wrapper');
+		teamSwiperSlides.forEach(slide => {
+			slide.classList.remove('team-swiper__slide');
+		});
+	} else if (windowWidth === 768) {
+		const teamSwiperWrapper = document.querySelector('.team-swiper .swiper-wrapper');
+		const teamSwiperSlides = Array.from(teamSwiperWrapper.children).slice(1);
+
+		teamSwiperWrapper.classList.add('team-swiper__wrapper');
+		teamSwiperSlides.forEach(slide => {
+			slide.classList.add('team-swiper__slide');
+		});
+	}
+});
+
 // JQUERY
